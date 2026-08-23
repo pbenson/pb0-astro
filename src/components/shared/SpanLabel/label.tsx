7cc017bcda0e0e17f69bc9e79ae1aa3c2df5f0ba
Pete Benson <pbenson@me.com>
2026-08-23 12:20:12 -0400

    chore: remove unused components left from the Docusaurus migration

--- src/components/shared/SpanLabel/label.tsx
+++
@@ -1,9 +0,0 @@
-interface SpanLabelProps {
-  label?: string | number;
-}
-
-export default function SpanLabel(props: SpanLabelProps) {
-  return (
-    <span>{props.label}</span>
-  );
-}
--- src/components/shared/label.tsx
+++
@@ -1,9 +0,0 @@
-interface LabelProps {
-  readonly label?: string | number;
-}
-
-export default function Label(props: LabelProps) {
-  return (
-    <span className="text--semibold">{props.label}&nbsp;</span>
-  );
-}
